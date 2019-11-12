7bed35ac0b6cbe55988d53557c7eee1729dc6b51
Hernand Azevedo <hernand.azevedo@gmail.com>
2019-11-12 07:48:16 -0300

    Initial Commit

---
+++ virtual-tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  virtual-tourist
+//
+//  Created by Hernand Azevedo on 12/11/19.
+//  Copyright © 2019 Hernand Azevedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
